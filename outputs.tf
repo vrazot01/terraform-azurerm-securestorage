228c81332e9711f754eb29389b6d4859219b4bb0
Tim Vrazo <vrazot@gmail.com>
2025-01-29 22:32:30 -0600

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "storage account_id" {
+  value       = azurerm_storage_account.securestorage
+  description = "SA id"
+}
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "resource_group_name" {
+  type        = string
+  description = "res group name"
+}
+variable "location" {
+  type        = string
+  description = "res location"
+}
+variable "storage_account_name" {
+  type        = string
+  description = "storage account name"
+}
+variable "environment" {
+  type        = string
+  description = "prod or dev"
+}
